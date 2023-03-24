c54e3d62b74a7ae90cc791f23d9a2efbe1e6d3c7
DanielRojas2002 <danymessi1000@gmail.com>
2023-03-24 09:57:33 -0600

    cambio

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,13 +1,4 @@
- // Boton para agregar contenido
- let btnAgregar = document.getElementById("btnAgregar");
- // Seleccion de la etiqueta main
- let main = document.getElementById("main");
- const texto = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio sequi nemo repudiandae delectus repellendus error dolorem ut quas eum minus maiores modi ducimus harum, facere dolor eligendi deleniti illum."
- let contador = 1;
- btnAgregar.addEventListener("click", ()=>{
-   main.innerHTML += `<p>${contador} ${texto}</p><br>`;
-   contador += 1;
- });
+
  // Cambiar la orientacion de la flecha
  let btnFooter = document.querySelector(".btnFooter");
  let simboloFlecha = document.querySelector(".simboloFlecha");
